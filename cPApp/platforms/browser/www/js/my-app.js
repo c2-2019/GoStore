93ecfe3aa849c5ee0b7bf07381597f7e7e7ecf7b
Maroun Melhem <maroun.melhem@gmail.com>
2019-11-26 23:06:03 +0200

    feat: Add api endpoints to login/register popups

--- cPApp/platforms/browser/www/js/my-app.js
+++ cPApp/platforms/browser/www/js/my-app.js
@@ -9,7 +9,7 @@ var isAndroid = Framework7.prototype.device.android === true;
 var isIos = Framework7.prototype.device.ios === true;
 
 // Add view
-var mainView = myApp.addView('.view-main', {
+myApp.addView('.view-main', {
     // Because we use fixed-through navbar we can enable dynamic navbar
     dynamicNavbar: true
 });
@@ -41,15 +41,64 @@ myApp.onPageInit('index', function (page) {
 (function ($) {
     "use strict";
 
+    var apiEndPoint = "";
+
     $(function () {
+        
         //Login action
         $("#do_action_login").on('click', function () {
+            var loginPopup = $('.login-screen');
 
+            var loginMsg = loginPopup.find('.custom_msg');
+            var loginLoader = loginPopup.find('.custom_loader');
+
+            var loginEndpoint = apiEndPoint + "";
+
+            var apiEmail = loginMsg.find('.email_input').val();
+            var apiPassword = loginMsg.find('.pw_input').val();
+
+            loginLoader.slideDown('fast');
+            loginMsg.html('Logging you in...');
+            loginMsg.slideDown('fast');
+
+            //Add API call
+            $.post(loginEndpoint, {
+                email: apiEmail,
+                password: apiPassword
+            }).done(function (ret1) {
+                if (ret1) {
+                    var retData = ret1['ret_data'];
+                }
+            }).fail(function () {
+            });
         });
 
-        //Login action
+        //Register action
         $("#do_action_register").on('click', function () {
-            alert('register');
+            var registerPopup = $('.popup-register');
+
+            var registerMsg = registerPopup.find('.custom_msg');
+            var registerLoader = registerPopup.find('.custom_loader');
+
+            var registerEndpoint = apiEndPoint + "";
+
+            var apiEmail = registerPopup.find('.email_input').val();
+            var apiPassword = registerPopup.find('.pw_input').val();
+
+            registerLoader.slideDown('fast');
+            registerMsg.html('Creating your new account...');
+            registerMsg.slideDown('fast');
+
+            //Add API call
+            $.post(registerEndpoint, {
+                email: apiEmail,
+                password: apiPassword
+            }).done(function (ret1) {
+                if (ret1) {
+                    var retData = ret1['ret_data'];
+                }
+            }).fail(function () {
+            });
         });
     });
 
