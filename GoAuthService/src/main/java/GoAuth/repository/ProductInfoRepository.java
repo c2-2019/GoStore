d07befd6b1dcdbf9f949ee9b1d01c65f35800cde
bardawilpeter <peterbardawil@gmail.com>
2019-11-29 16:13:53 +0200

    feat: fix product to inventory

---
+++ GoAuthService/src/main/java/GoAuth/repository/ProductInfoRepository.java
@@ -0,0 +1,21 @@
+package GoAuth.repository;
+
+import GoAuth.entity.ProductInfo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created By Peter.
+ */
+public interface ProductInfoRepository extends JpaRepository<ProductInfo, String> {
+    ProductInfo findByProductId(String id);
+
+    Page<ProductInfo> findAllByProductStatusOrderByProductIdAsc(Integer productStatus, Pageable pageable);
+
+    // product in one category
+    Page<ProductInfo> findAllByCategoryTypeOrderByProductIdAsc(Integer categoryType, Pageable pageable);
+
+    Page<ProductInfo> findAllByOrderByProductId(Pageable pageable);
+
+}
---
+++ GoAuthService/src/main/java/GoAuth/service/ProductInOrderService.java
@@ -0,0 +1,12 @@
+package GoAuth.service;
+
+import GoAuth.entity.ProductInOrder;
+import GoAuth.entity.User;
+
+/**
+ * Created By Zhu Lin on 1/3/2019.
+ */
+public interface ProductInOrderService {
+    void update(String itemId, Integer quantity, User user);
+    ProductInOrder findOne(String itemId, User user);
+}
